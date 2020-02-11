198ceb7d329546eeb94c7334a8d98b9b66296cb5
ndriqa <ndriqimrrahmani@hotmail.com>
2020-02-12 00:17:49 +0100

    Summation of primes below N

--- Project010/SummationOfPrimes.java
+++ Project010/SummationOfPrimes.java
@@ -1,4 +1,7 @@
 package Project010;
+
+import HelperPackage.PrimeNumbers;
+
 /**The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  *
  * Find the sum of all the primes below two million.
@@ -11,10 +14,25 @@ public class SummationOfPrimes {
     int MAX;
 
     public SummationOfPrimes(int max){
-        sum = 0;
+        sum = 2;          //start from the first prime since it's the only even prime
         MAX = max;
     }
     public void calculate(){
+        for(int i = 3; i < MAX; i = i + 2){     //we eliminate even numbers
+            if(PrimeNumbers.isPrime(i)){
+                sum+=i;
+            }
+        }
+    }
+    @Override
+    public String toString(){
+        return "the sum of prime numbers below " + MAX + " is: " + sum;
+    }
 
+    public static void main(String[] args) {
+        int max = 2000000;
+        SummationOfPrimes prisum = new SummationOfPrimes(max);
+        prisum.calculate();
+        System.out.println(prisum);
     }
 }
